90d61bf625422b44b61e00cb121981dfb62312d7
+ <nickson.mogaka@student.moringaschool.com>
2024-06-25 08:42:37 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+// Define companyName as a const with the value 'Scuber'
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood using let with the value 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let with the value 'Susan Smith'
+let companyCeo = 'Susan Smith';
+
+// Define currentUser with a sample value
+let currentUser = 'Grace Hopper';
+
+// Define welcomeMessage that contains "Welcome to Flatbook, " and the value of currentUser, ending with an exclamation point
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage that contains "WELCOME TO FLATBOOK, " and the value of currentUser in uppercase, ending with an exclamation point
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define shortGreeting that contains "Welcome, " and the first initial of currentUser, ending with an exclamation point
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
